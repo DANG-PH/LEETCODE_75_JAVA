325575f399dcd92a5ab34d1f722714e8e221baf6
DANG-PH <dangph.ptit@gmail.com>
2025-09-17 22:03:40 +0700

    New problem two pointer

---
+++ JAVA/Max_Number_of_K_Sum_Pairs_1679.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int ketQua = 0;
+        int left = 0, right = nums.length-1;
+        while (left < right) {
+            int tong = nums[left] + nums[right];
+            if (tong < k) {
+                left++;
+            } else if (tong > k) {
+                right--;
+            } else {
+                left++;
+                right--;
+                ketQua++;
+            }
+        }
+        return ketQua;
+    }
+}
+
+public class Max_Number_of_K_Sum_Pairs_1679 {
+}
